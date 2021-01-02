4b4c68bd3a7bc2d2857aef4321b8c929b3cc1a81
rihab <rihbalyasiri@yahoo.com>
2021-01-02 13:17:14 +0100

    adding constraints and alignment

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
+        diceImageView2.alpha = 0.5
+        
     }
 
 
+    @IBAction func rollButton(_ sender: Any) {
+    }
 }
 
